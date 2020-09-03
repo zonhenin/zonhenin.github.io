e9c72e417079bf4bd2fbf80aa2f0225b22af7a1d
Nelson Lee <nelsonleezonhat@gmail.com>
2020-09-03 22:46:33 +0930

    20200903

--- 锡庆里往事/web.js
+++ 锡庆里往事/web.js
@@ -1,27 +1,19 @@
 let defaultFonts='Helvetica, Tahoma, Arial, STXihei, "华文细黑", "Microsoft YaHei", "微软雅黑", sans-serif';
-let precise=false;
+
 function setPreferableFont() {
-	if (precise) {
-		return;
-	}
-	
-	let font='"BabelStone Han", '+defaultFonts;
 	$('.zhrr-st').each(function() {
 		$(this).text($(this).data('text'));
 	});
 	
-	let oh=$('.zhrr-text').height();
-	$('body').css('font-family', font);
-	let nh=$('.zhrr-text').height();
-	console.log(oh, nh);
+	let oh=$('.zhrr-text').height()
+	$('body').css('font-family', '"BabelStone Han", '+defaultFonts);
+	let nh=$('.zhrr-text').height()
 	if (oh==nh) {
 		setCompatibleFont();
 		toastr["error"]('载入专用字体失败。\n为正确显示所有专用吴语汉字，请<button type="button" id="download-font-button">下载专用字体</button>，並刷新页面。');
 		$('#download-font-button').click(function() {
 			window.location.href = 'BabelStoneHan.ttf';
 		});
-	} else {
-		precise=true;
 	}
 }
 
@@ -30,7 +22,6 @@ function setCompatibleFont() {
 		$(this).text($(this).data('alt'));
 	});	
 	$('body').css('font-family', defaultFonts);
-	precise=false;
 }
 
 $(function() {

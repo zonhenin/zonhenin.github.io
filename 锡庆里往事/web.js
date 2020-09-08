89b2f5c65261d28eef1bf693752a6d6582e6ca3b
Nelson Lee <nelsonleezonhat@gmail.com>
2020-09-08 19:26:57 +0930

    20200908

--- 锡庆里往事/web.js
+++ 锡庆里往事/web.js
@@ -1,5 +1,12 @@
 let defaultFonts='Helvetica, Tahoma, Arial, STXihei, "华文细黑", "Microsoft YaHei", "微软雅黑", sans-serif';
 let idealFont;
+var mobile
+
+function setMobile(e) {
+	if (mobile) {
+		e.text(e.data('mobile'));
+	}
+}
 
 function setPreferableFont() {
 	if (idealFont) {
@@ -8,6 +15,7 @@ function setPreferableFont() {
 	
 	$('.zhrr-st').each(function() {
 		$(this).text($(this).data('text'));
+		setMobile($(this));
 	});
 	
 	let oh=$('.zhrr-text').height();
@@ -17,7 +25,7 @@ function setPreferableFont() {
 		let nh=$('.zhrr-text').height();
 		if (oh==nh) {
 			setCompatibleFont();
-			toastr["error"]('载入专用字体失败。\n为正确显示所有专用吴语汉字，请<button type="button" id="download-font-button">下载专用字体</button>，並刷新页面。');
+			toastr["error"]('载入专用字体失败。\n为正确显示所有专用吴语汉字，请使用电脑上的火狐浏览器，或<button type="button" id="download-font-button">下载专用字体</button>，並刷新页面。');
 			$('#download-font-button').click(function() {
 				window.location.href = 'BabelStoneHan.ttf';
 			});
@@ -28,15 +36,36 @@ function setPreferableFont() {
 function setCompatibleFont() {
 	$('.zhrr-st').each(function() {
 		$(this).text($(this).data('alt'));
+		setMobile($(this));
 	});	
 	$('body').css('font-family', defaultFonts);
 	idealFont=false;
 }
 
+function wechat() {
+	let el = document.getElementById('wx_share');
+    el.target = '_new';
+    el.click();
+}
+
 $(function() {
 	$('#babel-stone').click(setPreferableFont);	
-	$('#safe-font').click(setCompatibleFont);
+	$('#safe-font').click(setCompatibleFont);	
 	
-	setCompatibleFont();
-	setPreferableFont();
+	{
+		let wg=$('#test-good').width();
+		let wh=$('#test-hard').width();
+		let wm=$('#test-mobile').width();
+		$('#test').remove();
+
+		mobile=wg==0 || wm/wg<0.7;
+		setCompatibleFont();	
+		if (wg!=0 && wh/wg>0.7) {
+			setPreferableFont();
+		}
+	}
+	
+	if (!mobile) {
+		setTimeout(wechat, 1000*60*2);
+	}
 });

bc40806abd4c036381c96492b8d1e4081ffb507c
Nelson Lee <nelsonleezonhat@gmail.com>
2021-04-17 11:09:53 +0930

    20210417

--- 锡庆里往事/web.js
+++ 锡庆里往事/web.js
@@ -1,16 +1,7 @@
-function wechat() {
-	let el = document.getElementById('wx_share');
-    el.target = '_new';
-    el.click();
-}
-
 $(function() {
 	$.blockUI({ message: $('#message-on-load'), css: { left: '25%', width: '50%' } });
 	$('#message-on-load-ok').click(function() { 
 		$.unblockUI();
-		if (!mobile) {
-			setTimeout(wechat, 1000*60*5);
-		}
 		$('audio')[0].play();
 	});
 });
